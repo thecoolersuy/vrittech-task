878906bd033c1f03705a061b27ebf57fbf3a9482
oatkrunch <suyalsahukhala787@gmail.com>
2026-05-25 23:54:50 +0545

    feat: initialize project with Vite, React Router, and clean boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Outlet } from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
+}
+
+export default App
